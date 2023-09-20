afbe404fc4c8b02e45f453e58de50a0f5f594a56
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-09-20 04:05:47 -0700

    updated

--- add.c
+++ add.c
@@ -1,23 +1,48 @@
 #include "monty.h"
 
 /**
- * m_add - adding the top two elements of the stack
+ * m_add - adding the top two elements of the
  */
 
-void m_add(stack_t **stack, unsigned int line_number)
-{
-	
-	int n = 0;
-struct node *pointer = (struct node *) malloc(sizeof(struct node));
-if (pointer == NULL)
-{
-	if (var.stack_len < 2)
-	{
-		/*dprintf(STDOUT_FILENO, "L%u: can't add, stack too short\n",line_number);*/
-		perror("cant add");
-		exit(EXIT_FAILURE);
-	}
-	n += (*stack)->n;
-	m_pop(stack, line_number);
-	(*stack)->n += n;
+int stack[STACK_SIZE];
+int top = -1;  // Initialize the top of the stack to -1 to indicate it's empty
+
+// Function to push an element onto the stack
+void push(int value) {
+    if (top == STACK_SIZE - 1) {
+        fprintf(stderr, "Stack overflow!\n");
+        exit(EXIT_FAILURE);
+    }
+    stack[++top] = value;
+}
+
+// Function to print all elements on the stack
+void pall() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d\n", stack[i]);
+    }
+}
+
+int main() {
+    char opcode[100];
+    int value, line_number = 0;
+
+    while (scanf("%s", opcode) != EOF) {
+        line_number++;
+
+        if (strcmp(opcode, "push") == 0) {
+            if (scanf("%d", &value) != 1) {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+            }
+            push(value);
+        } else if (strcmp(opcode, "pall") == 0) {
+            pall();
+        } else {
+            fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
 }
