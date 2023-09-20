eda27e739d07d7981cfb5fea271e8d4f39e31cad
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-09-20 04:00:52 -0700

    updated

--- add.c
+++ add.c
@@ -3,10 +3,14 @@
 /**
  * m_add - adding the top two elements of the stack
  */
+
 void m_add(stack_t **stack, unsigned int line_number)
 {
+	
 	int n = 0;
-
+struct node *pointer = (struct node *) malloc(sizeof(struct node));
+if (pointer == NULL)
+{
 	if (var.stack_len < 2)
 	{
 		/*dprintf(STDOUT_FILENO, "L%u: can't add, stack too short\n",line_number);*/
